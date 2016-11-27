28d2da2b315bd0fc0d17e6e63b1e42126f635412
Brandon Lundeen <Brandon Lundeen>
2016-11-27 16:37:54 -0500

    IC with 2.1

--- main.m
+++ main.m
@@ -30,17 +30,14 @@ for c=1:length(x)
      T(c)=1;
    
    elseif x(c)<1.5
-       rho(c)=1-3.66*(x(c)-.5);
+       rho(c)=1-.366*(x(c)-.5);
        T(c)=1-0.167*(x(c)-.5);
        
-   elseif x(c)<2.1
-       rho(c)=.634-.0702*(x(c)-1.5);
-       T(c)=.833-.4908*(x(c)-1.5);
        
        
-   elseif x(c)<3.1
-       rho(c)=.5896+.10228*(x(c)-2.1);
-       T(c)=.93968+.0622*(x(c)-2.1);
+   elseif x(c)<3.5
+       rho(c)=.634-.3879*(x(c)-1.5);
+       T(c)=.833-.3507*(x(c)-1.5);
        
    end
     
@@ -48,7 +45,7 @@ end
 
 V=.59./(rho.*A);
 
-all=struct('rho',rho,'T',T,'V',V,'U',empty3, 'F', empty3,'J', empty3);
+all=struct('rho',rho,'T',T,'V',V,'M',V./sqrt(T),'U',empty3, 'F', empty3,'J', empty3);
 dt=getdt(T,V,dx,CFL);
 
 all.U=encodeU(rho,A,V,T, dA);
@@ -56,4 +53,8 @@ all.U=encodeU(rho,A,V,T, dA);
 data(1)=all;
 
 %McCormack solver
+
+for c=1:N-1
+   Up(:,c)=-(all.F(:,c+1)-all.F(:,c))/dx-all.J(:,c); 
+end
 
